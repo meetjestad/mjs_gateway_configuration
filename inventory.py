cf3c2967adcd2806f8aff4afa5f3c02e26d6daee
Matthijs Kooijman <matthijs@stdin.nl>
2024-06-06 21:15:14 +0200

    inventory: Add
    This lists all currently used Meetjestad (lorank) gateways.

---
+++ inventory.py
@@ -0,0 +1,19 @@
+gateways = [
+    ("mjs-gateway-1", {}),
+    ("mjs-gateway-2", {}),
+    ("mjs-gateway-3", {}),
+    ("mjs-gateway-4", {
+        "4g_modem_dev":
+            "/dev/serial/by-id/usb-SimTech__Incorporated_SimTech__Incorporated_0123456789ABCDEF-if02-port0",
+        "4g_apn": "globaldata.iot",
+    }),
+    ("mjs-gateway-5", {}),
+    ("mjs-gateway-6", {}),
+    ("mjs-gateway-7", {}),
+    ("mjs-gateway-8", {}),
+    ("mjs-gateway-9", {}),
+]
+
+# TODO: Is there a nicer way for this?
+for name, data in gateways:
+    data['ssh_user'] = 'root'
